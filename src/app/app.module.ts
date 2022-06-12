430270ea68f72c2f2295f3d6341fe40226039fc5
maatougz <maatoug.medzouhaier@gmail.com>
2022-06-12 11:24:02 +0200

    template admin dashboard navbar & sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { FooterComponent } from './admin/footer/footer.component';
+import { NavbarComponent } from './admin/navbar/navbar.component';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
+import { ContentComponent } from './admin/content/content.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
+ 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
